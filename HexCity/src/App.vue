bbd91f94c89f79d3c1102ba49bc73a789c3b83fc
Evgeny Lazurin <king-of-Orbita@mail.ru>
2025-10-04 12:49:48 +0300

    Добавленная карта 2

---
+++ HexCity/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="map" style="width: 100%; height: 100vh"></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import DG from "2gis-maps";
+
+onMounted(() => {
+  const map = DG.map("map", {
+    center: [55.751244, 37.618423],
+    zoom: 11,
+    key: import.meta.env.VITE_2GIS_API_KEY,
+  });
+});
+</script>
